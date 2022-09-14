0708dcfc0ad9cebc95e12df662ad591aadb276cf
Daniel <84683727+danieldev-usr@users.noreply.github.com>
2022-09-14 17:23:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from ast import Pow
 import os
 import time
+import math
+
+
+#2
 
 def limpar():os.system("clear")
 
@@ -17,7 +22,10 @@ print("""
 [04] Somar massa (Newton)
 [05] Somar gastos eletricos (KwH)
 [06] Somar porcentagem da perda de peso
-[07] Feedbacks e erros
+[07] Transformação de medidas (Nao funcional...)
+[08] Porcentagem
+[09] Teorema de Pitagoras
+[00] Feedbacks e erros
 """)
 
 while True:
@@ -110,7 +118,60 @@ while True:
         print(f"{calc22} x 100 = {calc33}%")
         print(" ")
 
-    elif x == "07":
+    elif x == "00":
+        print(" ")
+        print("""
+        [1] Metro cubico para Decimetro cubico (m3/dm3)
+        [02] Centimetro cubico para Milimetro cubico (cm3/mm3)
+        [03] Decimetro cubico  para Milimetro cubico
+        [04] Mililitro para litro (ml/l)
+        [05] Litro para centilitro (l/cl)
+        [06] Litro para Mililitro (l/ml)
+        [07] Metro cubico para Litro (m3/l)
+        [08] Decimetro cubico para Litro (dm3/l)
+        [09] Centimetro cubico para mililitro (cm3/ml)
+
+        """)
+        lks = int(input("Digite a opçao: "))
+        if lks == "1":
+            vldpdt = int(input("Digite os metros: "))
+            cont1 = vldpdt * 1000
+            print(f"{cont1} DM3")
+
+
+    elif x == "08":
+        print(" ")
+        pv1 = float(input("Digite a porcentagem: "))
+        pv2 = float(input("Digite o valor para calcular: "))
+
+        rgdt = pv1 * pv2 / 100
+
+        print(rgdt, "%")
+
+
+    elif x == "09":
+        print(" ")
+        print("obs; apenas calculamos o valor da hipotenusa!!!")
+        cateto1 = float(input("Digite o valor do cateto B: "))
+        cateto2 = float(input("Digite o valor do cateto A: "))
+
+        sm1 = cateto1 * cateto1
+        sm2 = cateto2 * cateto2
+
+        sm3 = sm1 + sm2
+        math.sqrt(sm3)
+        print("a² = b² + c²")
+        print(f"a² = {cateto1}² + {cateto2}²")
+        print(f"a² = {sm1} + {sm2}")
+        print(f"a² = {sm3}")
+        print(f"a = √{sm3}")
+        a = math.sqrt(sm3)
+        print(f"a = {a}")
+
+
+
+
+    elif x == "00":
         print(" ")
         print("Instagram para contato: @metalheadkkkk")
         print("Telegram para contato: @BaalZevuv6")
@@ -120,19 +181,15 @@ while True:
         print("MIT license")
         print("""
         MIT License
-
 Copyright (c) 2022 Daniel 
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
